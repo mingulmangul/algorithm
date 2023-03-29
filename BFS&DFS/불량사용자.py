230f8264b8432fe2bab1531da82827ccbbf0977b
Min A <min2010011@gmail.com>
2023-03-29 22:56:03 +0900

    update: 14891, 불량사용자
    다른 풀이 추가

--- BFS&DFS/불량사용자.py
+++ BFS&DFS/불량사용자.py
@@ -9,6 +9,10 @@ DFS
 **
 - product 또는 permutation을 사용하면 쉽게 조합을 구할 수 있음!
 """
+from itertools import product
+
+# 내 풀이 (DFS로 조합 선택)
+# 가장 시간이 오래걸리는 테스트 5번에서 47.77ms
 banned_list = []
 
 
@@ -43,5 +47,32 @@ def solution(user_id, banned_id):
     return len(banned_list)
 
 
-print(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"],
-               ["fr*d*", "*rodo", "******", "******"]))
+# 추가 풀이 (product 함수로 조합 선택)
+# 테스트 5번에서 6123.84ms
+def solution2(user_id, banned_id):
+    candidates = [[] for _ in range(len(banned_id))]
+
+    # 제재 아이디 - [제재 아이디가 될 수 있는 아이디 목록] 매핑
+    for idx, bid in enumerate(banned_id):
+        for uid in user_id:
+            if len(bid) != len(uid):
+                continue
+            for i in range(len(bid)):
+                if bid[i] != "*" and bid[i] != uid[i]:
+                    break
+            else:
+                candidates[idx].append(uid)
+
+    # 조합 선택
+    combs = product(*candidates)
+    for comb in combs:
+        comb = set(comb)
+        if len(comb) == len(banned_id) and comb not in banned_list:
+            banned_list.append(comb)
+
+    print(banned_list)
+    return len(banned_list)
+
+
+print(solution2(["frodo", "fradi", "crodo", "abc123", "frodoc"],
+                ["fr*d*", "*rodo", "******", "******"]))
--- 구현/14891.py
+++ 구현/14891.py
@@ -5,20 +5,24 @@
 import sys
 input = sys.stdin.readline
 
-# 톱니바퀴 클래스
+################################
+# 톱니바퀴를 클래스로 처리한 풀이
+################################
+
+
 class wheel:
     # 테스트용 출력
     def __str__(self):
         temp = self.state[self.top:] + self.state[:self.top]
         return str(temp)
-    
+
     def __init__(self, data):
         self.state = [x for x in data]  # 톱니바퀴 극 상태 정보
         self.top = 0                    # 12시 방향의 인덱스
-    
+
     def get_left(self):
         return self.state[(self.top + 6) % 8]   # 왼쪽의 극 리턴
-        
+
     def get_right(self):
         return self.state[(self.top + 2) % 8]   # 오른쪽의 극 리턴
 
@@ -27,7 +31,7 @@ class wheel:
             self.top = (self.top + 1) % 8   # 12시 방향의 인덱스를 모듈러 연산
         elif direction == 1:                # 시계방향 회전
             self.top = (self.top + 7) % 8
-            
+
 
 info = [input().rstrip() for _ in range(4)]
 wheels = [wheel(data) for data in info]
@@ -35,7 +39,7 @@ K = int(input().rstrip())
 for _ in range(K):
     target, direction = map(int, input().split())
     target -= 1
-    
+
     directions = [0] * 4    # 각 톱니바퀴가 회전해야 할 방향
     directions[target] = direction
     # 오른쪽에 있는 톱니바퀴 중 회전해야 하는 톱니바퀴가 있는지 확인
@@ -60,3 +64,41 @@ for _ in range(K):
 # 출력
 # 12시 방향이 S극(1)인 경우에만 점수 획득
 print(sum([2**i for i in range(4) if wheels[i].state[wheels[i].top] == '1']))
+
+
+################################
+# 톱니바퀴를 문자열로 처리한 풀이
+################################
+"""
+wheels = [input().rstrip() for _ in range(4)]
+K = int(input().rstrip())
+
+for _ in range(K):
+    target, direction = map(int, input().split())
+    rotation = [0] * 4  # rotation[i] : i번째 톱니바퀴의 회전할 방향
+    rotation[target-1] = direction
+    # 오른쪽 톱니바퀴 확인
+    for i in range(target-1, 3):
+        # 현재 톱니바퀴의 오른쪽 != 오른쪽 톱니바퀴의 왼쪽
+        if wheels[i][2] != wheels[i+1][6]:
+            # 오른쪽 톱니바퀴 회전
+            rotation[i+1] = -rotation[i]
+        else:
+            break
+    # 왼쪽 톱니바퀴 확인
+    for i in range(target-1, 0, -1):
+        # 현재 톱니바퀴의 왼쪽 != 왼쪽 톱니바퀴의 오른쪽
+        if wheels[i][6] != wheels[i-1][2]:
+            rotation[i-1] = -rotation[i]
+        else:
+            break
+
+    # 실제 회전
+    for i in range(4):
+        if rotation[i] != 0:
+            top = 1 if rotation[i] == -1 else 7
+            wheels[i] = wheels[i][top:] + wheels[i][:top]
+
+# 점수 계산
+print(sum([int(wheels[i][0]) * 2 ** i for i in range(4)]))
+"""
